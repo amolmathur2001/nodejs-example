6bf5e2473e5a00c09fd493e18b6d8b4bd4f68c3f
Amol Mathur <amolmathur2001@gmail.com>
2024-09-08 12:36:38 +0530

    minor change in deps

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,25 +2,26 @@
 
 # Navigate to the application directory
 cd /var/www/nodejs-example
+echo "Moved to nodejs-example directory"
 
 # Install nodejs
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# sudo apt-get install -y nodejs
 
 # Install npm
-sudo apt install npm
+# sudo apt install npm
 
 # Install application dependencies
-npm install
+# npm install
 
 # Install PM2 globally if it's not already installed
-if ! command -v pm2 &> /dev/null
-then
-    echo "PM2 could not be found. Installing PM2..."
-    npm install -g pm2
-else
-    echo "PM2 is already installed."
-fi
+#if ! command -v pm2 &> /dev/null
+#then
+#    echo "PM2 could not be found. Installing PM2..."
+#    npm install -g pm2
+#else
+#    echo "PM2 is already installed."
+#fi
 
 
 # Optional: Run any additional setup commands if needed
