18cf83b507f79e761e92fa0a321502d58261ae2f
Amol Mathur <amolmathur2001@gmail.com>
2024-09-08 21:41:14 +0530

    update code in print remove h1

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const server = http.createServer((req, res) => {
     // Set the response HTTP header with HTTP status and Content type
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     
-    // Send the response body "Hello, World!"
-    res.end('<h1>This My web application </h1>');
+    // Send the response body
+    res.end('This My web application');
 });
 
 // Server listens on port 3000
